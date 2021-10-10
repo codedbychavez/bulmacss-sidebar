3ec45427fb819720339e95b0a8f44a97e14278b2
Chavez Harris <74829200+softiosolutions@users.noreply.github.com>
2021-10-10 17:05:46 -0400

    added active class tracking

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  public menuItems = [
+    {
+      "name": "Team Settings",
+      "children": []
+    },
+    {
+      "name": "Manage Your Team",
+      "children": [
+        {
+          "name": "Members"
+        },
+        {
+          "name": "plugins"
+        },
+        {
+          "name": "Add a member"
+        }
+      ]
+    },
+    {
+      "name": "Invations",
+      "children": []
+    },
+    {
+      "name": "Cloud Storage Environment Settings",
+      "children": []
+    },
+    {
+      "name": "Authentication",
+      "children": []
+    }
+  ]
+
+  // Set to -1 to prevent the first menu item from being selected
+  public active = -1;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -8,6 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class TopNavComponent implements OnInit {
   public sideBarActive: boolean;
 
+  public menuItems = [
+    {
+      "name": "Team Settings",
+      "children": []
+    },
+    {
+      "name": "Manage Your Team",
+      "children": [
+        {
+          "name": "Members"
+        },
+        {
+          "name": "plugins"
+        },
+        {
+          "name": "Add a member"
+        }
+      ]
+    },
+    {
+      "name": "Invations",
+      "children": []
+    },
+    {
+      "name": "Cloud Storage Environment Settings",
+      "children": []
+    },
+    {
+      "name": "Authentication",
+      "children": []
+    }
+  ]
+
+  // Set to -1 to prevent the first menu item from being selected
+  public active = -1;
+
   constructor() { 
     this.sideBarActive = false;
    }
